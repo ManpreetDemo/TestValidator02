6f668d1ecccd2bdbcb679c276b6dc7c8fa3b8882
ManpreetDemo <52816266+ManpreetDemo@users.noreply.github.com>
2021-03-25 09:34:57 +0000

    Update Program.cs

--- MG.SedolValidator.Console/Program.cs
+++ MG.SedolValidator.Console/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MG.SedolValidator
 {
@@ -28,10 +25,25 @@ namespace MG.SedolValidator
             Console.ReadKey();
         }
 
+        private static void DisplayScenario(string scenarioTitle, string scenarioDescription, List<string> scenarioList)
+        {
+            Console.WriteLine(scenarioTitle);
+            Console.WriteLine(scenarioDescription);
+
+            Console.WriteLine(formatScenario, "InputString Test Value", "IsValidSedol", "IsUserDefined", "ValidationDetails");
+            Console.WriteLine(new String('-', 100));
+
+            foreach (var scenario in scenarioList)
+            {
+                var sArr = scenario.Split('|');
+                var result = new ValidationResult(sArr[0], Convert.ToBoolean(sArr[1]), Convert.ToBoolean(sArr[2]), sArr[3]);
+                Console.WriteLine(formatScenario, result.InputString, result.IsValidSedol, result.IsUserDefined, result.ValidationDetails);
+            }
+
+        }
+
         private static void Scenario1()
         {
-            Console.WriteLine("*** Scenario 1 ***\n");
-            Console.WriteLine("Null, empty string or string other than 7 characters long\n");
             List<string> scenarioList = new List<string>
             {
                 "Null|False|False|Input string was not 7-characters long",
@@ -39,115 +51,57 @@ namespace MG.SedolValidator
                 "12|False|False|Input string was not 7-characters long",
                 "123456789|False|False|Input string was not 7-characters long"
             };
-            Console.WriteLine(formatScenario, "InputString Test Value", "IsValidSedol", "IsUserDefined", "ValidationDetails");
-            Console.WriteLine(new String('-', 100));
 
-            foreach (var scenario in scenarioList)
-            {
-                var sArr = scenario.Split('|');
-                var result = new ValidationResult(sArr[0], Convert.ToBoolean(sArr[1]), Convert.ToBoolean(sArr[2]), sArr[3]);
-                Console.WriteLine(formatScenario, result.InputString, result.IsValidSedol, result.IsUserDefined, result.ValidationDetails);
-            }
-        }
+            DisplayScenario(scenarioTitle: "*** Scenario 1 ***\n", scenarioDescription: "Null, empty string or string other than 7 characters long\n", scenarioList: scenarioList);
+        }      
 
         private static void Scenario2()
         {            
-            Console.WriteLine("*** Scenario 2 ***\n");
-            Console.WriteLine("Invalid Checksum non user defined SEDOL\n");
             List<string> scenarioList = new List<string>
             {
                 "1234567|False|False|Checksum digit does not agree with the rest of the input"
             };
-            Console.WriteLine(formatScenario, "InputString Test Value", "IsValidSedol", "IsUserDefined", "ValidationDetails");
-            Console.WriteLine(new String('-', 100));
-
-            foreach (var scenario in scenarioList)
-            {
-                var sArr = scenario.Split('|');
-                var result = new ValidationResult(sArr[0], Convert.ToBoolean(sArr[1]), Convert.ToBoolean(sArr[2]), sArr[3]);
-                Console.WriteLine(formatScenario, result.InputString, result.IsValidSedol, result.IsUserDefined, result.ValidationDetails);
-            }
-
+            DisplayScenario(scenarioTitle: "*** Scenario 2 ***\n", scenarioDescription: "Invalid Checksum non user defined SEDOL\n", scenarioList: scenarioList);
         }
 
         private static void Scenario3()
-        {
-            Console.WriteLine("*** Scenario 3 ***\n");
-            Console.WriteLine("Valid non user define SEDOL\n");
+        {            
             List<string> scenarioList = new List<string>
             {
                 "0709954|True|False|Null",
                 "B0YBKJ7|True|False|Null"
             };
-            Console.WriteLine(formatScenario, "InputString Test Value", "IsValidSedol", "IsUserDefined", "ValidationDetails");
-            Console.WriteLine(new String('-', 100));
-
-            foreach (var scenario in scenarioList)
-            {
-                var sArr = scenario.Split('|');
-                var result = new ValidationResult(sArr[0], Convert.ToBoolean(sArr[1]), Convert.ToBoolean(sArr[2]), sArr[3]);
-                Console.WriteLine(formatScenario, result.InputString, result.IsValidSedol, result.IsUserDefined, result.ValidationDetails);
-            }
+            DisplayScenario(scenarioTitle: "*** Scenario 3 ***\n", scenarioDescription: "Valid non user define SEDOL\n", scenarioList: scenarioList);
         }
 
         private static void Scenario4()
         {
-            Console.WriteLine("*** Scenario 4 ***\n");
-            Console.WriteLine("Invalid Checksum user defined SEDOL\n");
             List<string> scenarioList = new List<string>
             {
                 "9123451|False|True|Checksum digit does not agree with the rest of the input",
                 "9ABCDE8|False|True|Checksum digit does not agree with the rest of the input"
             };
-            Console.WriteLine(formatScenario, "InputString Test Value", "IsValidSedol", "IsUserDefined", "ValidationDetails");
-            Console.WriteLine(new String('-', 100));
-
-            foreach (var scenario in scenarioList)
-            {
-                var sArr = scenario.Split('|');
-                var result = new ValidationResult(sArr[0], Convert.ToBoolean(sArr[1]), Convert.ToBoolean(sArr[2]), sArr[3]);
-                Console.WriteLine(formatScenario, result.InputString, result.IsValidSedol, result.IsUserDefined, result.ValidationDetails);
-            }
+            DisplayScenario(scenarioTitle: "*** Scenario 4 ***\n", scenarioDescription: "Invalid Checksum user defined SEDOL\n", scenarioList: scenarioList);
         }
 
         private static void Scenario5()
-        {
-            Console.WriteLine("*** Scenario 5 ***\n");
-            Console.WriteLine("Invaid characters found\n");
+        {            
             List<string> scenarioList = new List<string>
             {
                 "9123_51|False|False|SEDOL contains invalid characters",
                 "VA.CDE8|False|False|SEDOL contains invalid characters"
             };
-            Console.WriteLine(formatScenario, "InputString Test Value", "IsValidSedol", "IsUserDefined", "ValidationDetails");
-            Console.WriteLine(new String('-', 100));
-
-            foreach (var scenario in scenarioList)
-            {
-                var sArr = scenario.Split('|');
-                var result = new ValidationResult(sArr[0], Convert.ToBoolean(sArr[1]), Convert.ToBoolean(sArr[2]), sArr[3]);
-                Console.WriteLine(formatScenario, result.InputString, result.IsValidSedol, result.IsUserDefined, result.ValidationDetails);
-            }
+            DisplayScenario(scenarioTitle: "*** Scenario 5 ***\n", scenarioDescription: "Invaid characters found\n", scenarioList: scenarioList);
         }
 
         private static void Scenario6()
         {
-            Console.WriteLine("*** Scenario 6 ***\n");
-            Console.WriteLine("Valid user defined SEDOL\n");
             List<string> scenarioList = new List<string>
             {
                 "9123458|True|True|Null",
                 "9ABCDE1|True|True|Null"
             };
-            Console.WriteLine(formatScenario, "InputString Test Value", "IsValidSedol", "IsUserDefined", "ValidationDetails");
-            Console.WriteLine(new String('-', 100));
-
-            foreach (var scenario in scenarioList)
-            {
-                var sArr = scenario.Split('|');
-                var result = new ValidationResult(sArr[0], Convert.ToBoolean(sArr[1]), Convert.ToBoolean(sArr[2]), sArr[3]);
-                Console.WriteLine(formatScenario, result.InputString, result.IsValidSedol, result.IsUserDefined, result.ValidationDetails);
-            }
+            DisplayScenario(scenarioTitle: "*** Scenario 6 ***\n", scenarioDescription: "Valid user defined SEDOL\n", scenarioList: scenarioList);
         }
        
     }
